e530f845bb8f91757527b063874548de991e65ac
SepehrGanji <sepehrganji79@gmail.com>
2021-08-30 15:36:19 +0430

    Adding submit url

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.end('Hello Wordl!');
+app.get('/', (_, res) => {
+    res.end('Hello World!');
+});
+
+app.post('/submit', fileUpload({
+    limits: { fileSize: 20 * 1024 * 1024 }
+}), (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    const codeFile = req.files.codeFile;
+    const fileExtension = codeFile.name.split('.').pop();
+    if(!(fileExtension === 'cpp' || fileExtension === 'py' || fileExtension === 'java')) {
+        return res.status(400).send('Invalid file extension.');
+    }
+    const uniqueId = new Date().getTime().toString() + '-' + Math.floor(Math.random() * 1000).toString();
+    const uploadPath = `${__dirname}/uploads/${uniqueId + '.' + fileExtension}`;
+    codeFile.mv(uploadPath, (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send('File uploaded!'); //TODO: return _id
+    });
 });
 
 app.listen(port, () => {
