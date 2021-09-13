69e028115dcfb921bb910a520c5e8f1869c9df81
SepehrGanji <sepehrganji79@gmail.com>
2021-09-13 13:06:33 +0430

    Adding scheduler

--- utils.js
+++ utils.js
@@ -1,13 +1,35 @@
-const compileCode = (_id, code) => {
-    console.log('compiling code...');
-    console.log(_id, code);
+const schedule = require('node-schedule');
+
+const resourceChecker = mode => {
     //TODO
+    return true;
+};
+
+const compileCode = (_id, code) => {
+    const haveResource = resourceChecker('COMPILE');
+    if (haveResource) {
+        console.log('Compiling code...');
+        console.log(_id, code); //TODO: API Call
+    } else {
+        console.log('No Resource for compile...');
+        const currentDate = new Date();
+        const futureDate = new Date(currentDate.getTime() + (60 * 1000) + Math.random() * 10);
+        schedule.scheduleJob(futureDate, () => compileCode(_id, code));
+    }
 }
 
 const runMatch = (match_id, code1, code2) => {
-    console.log('running match...');
-    console.log(match_id, code1, code2); //TODO: Adding team names
-    //TODO
+    const haveResource = resourceChecker('MATCH');
+    if (haveResource) {
+        console.log('Running match...');
+        //TODO: Adding team names
+        console.log(match_id, code1, code2); //TODO: API Call
+    } else {
+        console.log('No Resource for match...');
+        const currentDate = new Date();
+        const futureDate = new Date(currentDate.getTime() + (120 * 1000) + Math.random() * 10);
+        schedule.scheduleJob(futureDate, () => runMatch(_id, code));
+    }
 }
 
 module.exports = {
